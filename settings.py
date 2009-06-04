e82f482827bb74004a0555dd7691fafe9054f712
2shortplanks <mark@twoshortplanks.com>
2009-06-04 17:52:26 +0100

    more getting admin interface to work, getting basic template rendering to work

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 # Django settings for tsp project.
 
+import os
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -69,6 +71,9 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    
+    # templates are in the templates dir in the same directory as this file
+    os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
 )
 
 INSTALLED_APPS = (
--- urls.py
+++ urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover();
+
+# some of the more basic pages just go straight to a template
+from django.views.generic.simple import *
 
 urlpatterns = patterns('',
     # Example:
@@ -10,8 +13,12 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/(.*)', admin.site.root),
+    (r'^admin/(.*)', admin.site.root),
+    
+    # the homepage and other simple pages
+    (r'^$', direct_to_template, { 'template': 'index.html' }),
+    (r'^about', direct_to_template, { 'template': 'about.html' }),
 )
