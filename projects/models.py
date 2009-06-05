62a3221045f6d8938cbcda3cfe56abe5a80b9330
2shortplanks <mark@twoshortplanks.com>
2009-06-05 18:08:28 +0100

    gave useful __unicode__ descriptions

--- projects/models.py
+++ projects/models.py
@@ -4,6 +4,8 @@ class ProjectSection(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField()
     parent = models.ForeignKey('ProjectSection', null=True, blank=True)
+    def __unicode__(self):
+        return self.name
 
 class Project(models.Model):
     name = models.CharField(max_length=200)
@@ -11,4 +13,6 @@ class Project(models.Model):
     synopsis = models.CharField(max_length=255)
     pub_date = models.DateTimeField('date published')
     section = models.ForeignKey(ProjectSection)
+    def __unicode__(self):
+        return self.name
 
