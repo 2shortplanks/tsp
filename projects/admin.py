9fc85f4f079a189c7fb136d51972925689ae6d81
2shortplanks <mark@twoshortplanks.com>
2009-06-05 18:15:03 +0100

    add a custom admin class, and make the slug fields autopopulate

--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,11 @@
 from tsp.projects.models import *
 from django.contrib import admin
 
-admin.site.register(Project)
-admin.site.register(ProjectSection)
+class ProjectAdmin(admin.ModelAdmin):
+  prepopulated_fields = {"slug": ("name",)}
+
+class ProjectSectionAdmin(admin.ModelAdmin):
+  prepopulated_fields = {"slug": ("name",)}
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(ProjectSection, ProjectSectionAdmin)
