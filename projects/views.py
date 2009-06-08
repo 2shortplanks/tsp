844b5216e4c3aea83295f0cab816b655c2237e42
2shortplanks <mark@twoshortplanks.com>
2009-06-08 15:44:13 +0100

    Make a proper page for projects with breadcrumbs

--- projects/views.py
+++ projects/views.py
@@ -2,10 +2,12 @@ from django.http import HttpResponse
 
 from chouwa.shortcuts import direct_to_template
 
+from constants import home
 
 def index(request):
-    return direct_to_template(request, "foo")
-
+    return direct_to_template(request, "projects.html", {
+       'breadcrumb': [ home, { 'title': "Projects" } ] 
+    })
 def detail(request, project_tag):
     return HttpResponse("detail")
 
