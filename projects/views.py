a3b786d25eb7d6129466782caa59138a68aecf74
2shortplanks <mark@twoshortplanks.com>
2009-06-12 07:27:25 +0100

    reworked templates dir structure

--- projects/views.py
+++ projects/views.py
@@ -9,19 +9,20 @@ from django.core.exceptions import ObjectDoesNotExist
 from constants import home
 
 def index(request):
-    return direct_to_template(request, "projects.html", {
+    return direct_to_template(request, "projects/index.html", {
        'breadcrumb': [ home, { 'title': "Projects" } ] 
     })
 
 def section_detail(request, fragment):
     ps = get_object_or_404(ProjectSection,slug=fragment)
-    return direct_to_template(request, "projects_category_detail.html", {
-       'breadcrumb': [ home, {}, { 'title': ps.name } ] 
+    return direct_to_template(request, "projects/category_detail.html", {
+       'breadcrumb': [ home, {}, { 'title': ps.name } ],
+       'section': ps
     })
 
 def project_detail(request, fragment):
     proj = get_object_or_404(Project,slug=fragment)
-    return direct_to_template(request, "projects_project_detail.html", {
+    return direct_to_template(request, "projects/project_detail.html", {
        'breadcrumb': [ home, {}, { 'title': proj.name } ] 
     })
 
