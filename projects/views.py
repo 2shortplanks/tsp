969c4cbb5f17308ff5b1a40cf790719fd6c0b106
2shortplanks <mark@twoshortplanks.com>
2009-06-12 07:24:48 +0100

    move to using get_object_or_404

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 
 from chouwa.shortcuts import direct_to_template
+from django.shortcuts import get_object_or_404
 
 from projects.models import *
 from django.core.exceptions import ObjectDoesNotExist
@@ -13,15 +14,15 @@ def index(request):
     })
 
 def section_detail(request, fragment):
-    ps = ProjectSection.objects.get(slug=fragment)
+    ps = get_object_or_404(ProjectSection,slug=fragment)
     return direct_to_template(request, "projects_category_detail.html", {
        'breadcrumb': [ home, {}, { 'title': ps.name } ] 
     })
 
 def project_detail(request, fragment):
-    ps = Project.objects.get(slug=fragment)
+    proj = get_object_or_404(Project,slug=fragment)
     return direct_to_template(request, "projects_project_detail.html", {
-       'breadcrumb': [ home, {}, { 'title': ps.name } ] 
+       'breadcrumb': [ home, {}, { 'title': proj.name } ] 
     })
 
 
