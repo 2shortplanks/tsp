36a170b9932676731a30441ced9d24f558d2e510
2shortplanks <mark@twoshortplanks.com>
2009-06-13 21:23:27 +0100

    get breadcrumbs working - no urls yet

--- projects/views.py
+++ projects/views.py
@@ -13,17 +13,32 @@ def index(request):
        'breadcrumb': [ home, { 'title': "Projects" } ] 
     })
 
+def breadcrumb_for_project_section(section):
+    if section.parent:
+        result = breadcrumb_for_project_section(section.parent)
+    else:
+        result = [ home ]
+    result.append({ 'title': section.name, 'url': 'clickme' })
+    return result
+
 def section_detail(request, fragment):
     ps = get_object_or_404(ProjectSection,slug=fragment)
     return direct_to_template(request, "projects/category_detail.html", {
-       'breadcrumb': [ home, {}, { 'title': ps.name } ],
+       'breadcrumb': breadcrumb_for_project_section(ps),
        'section': ps
     })
+
+def breadcrumb_for_project(project):
+    ps = project.section
+    breadcrumb = breadcrumb_for_project_section(ps)
+    breadcrumb.append({ 'title': project.name, 'url':'clickclick'})
+    return breadcrumb
 
 def project_detail(request, fragment):
     proj = get_object_or_404(Project,slug=fragment)
     return direct_to_template(request, "projects/project_detail.html", {
-       'breadcrumb': [ home, {}, { 'title': proj.name } ] 
+       'breadcrumb': breadcrumb_for_project(proj),
+       'project': proj
     })
 
 
